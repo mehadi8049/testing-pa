3278ed8ed24f46b1c39716af2fbce0d4f08e28ab
mehadi8049 <mehadi8049@gmail.com>
2024-04-04 15:30:53 +0600

    sdfsdfsdds

--- src/TestServiceProvider.php
+++ src/TestServiceProvider.php
@@ -18,7 +18,7 @@ class TestServiceProvider extends ServiceProvider
         });
 
         $this->mergeConfigFrom(
-            __DIR__.'/../config/Test.php', 'test'
+            __DIR__.'/../config/test.php', 'test'
         );
     }
 
@@ -30,7 +30,7 @@ class TestServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/Test.php' => config_path('Test.php'),
+            __DIR__.'/../config/test.php' => config_path('test.php'),
         ]);
     }
 }
